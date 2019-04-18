d0bfb9005c134f1f86e7d252212e48df6cbaea00
Adam Shannon <adamkshannon@gmail.com>
2019-04-18 12:20:54 -0500

    cmd/apitest: add logic for verifying paygate merged files after fake transfer generation
    As part of paygate's work to merge transfers for more efficient ACH
    uploads to the Fed we need to verify the files contain exactly the
    transfers which have been created.
    
    See: https://github.com/moov-io/paygate/pull/96

---
+++ cmd/apitest/verify.go
@@ -0,0 +1,111 @@
+// Copyright 2019 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/moov-io/ach"
+)
+
+func verifyDirIsEmpty(dir string) bool {
+	s, err := os.Stat(dir)
+	if err != nil || !s.IsDir() {
+		if os.IsNotExist(err) {
+			return true // dir doesn't exist
+		}
+		return false // dir doesnisn't a directory
+	}
+	fd, err := os.Open(dir)
+	if err != nil {
+		return false
+	}
+	names, err := fd.Readdirnames(1)
+	if (err != nil && err != io.EOF) || len(names) > 0 {
+		return false // found a file, so not empty
+	}
+	return true
+}
+
+// verifyTransfersWereMerged will take the incoming iterations (i.e. Transfers and related metadata) to
+// verify all transfers exist in the merged ACH files in dir. This is done to help ensure paygate handles
+// and uploads all the given transfers to the FED / receiving FI.
+func verifyTransfersWereMerged(dir string, iterations []*iteration) error {
+	if len(iterations) == 0 {
+		return fmt.Errorf("no iterations (transfers) found")
+	}
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if (err != nil && err != filepath.SkipDir) || info.IsDir() {
+			return nil // Ignore SkipDir and directories
+		}
+
+		file, err := parseACHFilepath(path)
+		if err != nil {
+			return fmt.Errorf("error reading %s: %v", path, err)
+		}
+		i := -1
+		for {
+			i++
+			if i >= len(iterations) {
+				break
+			}
+			if file.Header.ImmediateOrigin != iterations[i].originatorDepository.RoutingNumber {
+				continue
+			}
+			if file.Header.ImmediateDestination != iterations[i].customerDepository.RoutingNumber {
+				continue
+			}
+			// Check file's batches
+			for j := range file.Batches {
+				entries := file.Batches[j].GetEntries()
+				for k := range entries {
+					// TODO(adam): Once/If we move paygate's Amount into a shared lib we can do a better comparison here
+					if iterations[i].transfer.Amount == fmt.Sprintf("USD %.2f", float64(entries[k].Amount)/100.0) {
+						log.Printf("INFO: Matched transfer %s for %s", iterations[i].transfer.Id, iterations[i].transfer.Amount)
+						// found a match // TODO(adam): compare more fields?
+						iterations = append(iterations[:i], iterations[i+1:]...) // remove iteration
+						i = -1                                                   // reset i so we reprocess entire iterations array
+						goto next
+					}
+				}
+			}
+		next:
+			// check the next iteration, iterations leftover are those that weren't found in a file
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	if len(iterations) > 0 {
+		var transferLine []string
+		for i := range iterations {
+			transferLine = append(transferLine, fmt.Sprintf("%s (amount: %s)", iterations[i].transfer.Id, iterations[i].transfer.Amount))
+		}
+		return fmt.Errorf("transfers not matched!!\n%s", strings.Join(transferLine, "\n"))
+	} else {
+		log.Printf("SUCCESS: all transfers matched in merged file(s)")
+	}
+	return nil
+}
+
+func parseACHFilepath(path string) (*ach.File, error) {
+	fd, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	file, err := ach.NewReader(fd).Read()
+	if err != nil {
+		return nil, err
+	}
+	return &file, nil
+}
